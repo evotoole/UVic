71577ae2fc85ccf02525798825df43decacc1d66
evanotoole123 <evankarlotoole@gmail.com>
2025-01-18 12:16:37 -0800

    Some errors fixed. Program can now properly respond to 301 and 302 errors

--- CSC361/Assignment1/WebTester.py
+++ CSC361/Assignment1/WebTester.py
@@ -13,7 +13,7 @@ if (len(sys.argv) != 2):
 URI = sys.argv[1]
 
 #first attempt to connect via HTTP
-def connect_http(URI):
+def connect_http(URI, port = HTTP_PORT):
 
     '''
     -HTTP specifically relies on using TCP in the transport layer.
@@ -26,8 +26,9 @@ def connect_http(URI):
     -SOCK_STREAM specifies that we want to use TCP. (we could also use UDP)
     But of course not for HTTP.
     '''
-
+  
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   
 
     '''
     80 is the port number for HTTP
@@ -62,31 +63,37 @@ def connect_http(URI):
     response = client_socket.recv(10000).decode('utf-8')
     if ('301' in response) or ('302' in response):
         if "Location: https" in response:
-            connect_https(get_location(response))
+            check_http2(get_location(response))
 
     print(response)
 
 
 #if needed connect via HTTPS
-def connect_https(URI):
-    if 'https' in URI:
-        URI = URI[8:-1]
-    print('---------------------')
-    print(URI)
+def check_http2(URI): 
     context = ssl.create_default_context()
     context.set_alpn_protocols(['h2','http/1.1'])
     client_socket = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=URI)
     client_socket.connect((URI, HTTPS_PORT))
-    negotiated_protocol = client_socket.selected_alpn_protocol()
-  
-    print(negotiated_protocol)
+    request = f"OPTIONS / HTTP/1.1\r\n"
+    request += f"Host:{URI}\r\n"
+    request += f"Connection: keep-alive\r\n"
+    request += f"Upgrade: h2c\r\n"
+    request += f"Accept: */*\r\n"
+    request += f"\r\n"
+    client_socket.send(request.encode('utf-8'))
+    response = client_socket.recv(10000).decode('utf-8')
+    print(response)
+
+
+    #negotiated_protocol = client_socket.selected_alpn_protocol()
+    #print(negotiated_protocol)
 
 
 def get_location(response):
     pattern = 'Location: http.+/'
     new_uri = re.findall(pattern, response)
     new_uri = new_uri[0] 
-    new_uri = new_uri[10:]
+    new_uri = new_uri[18:-1]
     return (new_uri)
 
 connect_http(URI)
