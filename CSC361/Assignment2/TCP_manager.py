ffa9ceda982ac603660209d4ab955347c3ee7caf
evanotoole123 <evankarlotoole@gmail.com>
2025-03-07 17:27:11 -0800

    Assignment 2 nearly complete. Still must further test and do RTT computations

--- CSC361/Assignment2/TCP_manager.py
+++ CSC361/Assignment2/TCP_manager.py
@@ -15,11 +15,206 @@ BIG_END_MAGIC_NUM = b'\xa1\xb2\xc3\xd4'
 #takes a list of dictionaries corresponding to the packets from the cap file
 #organizes list dictionary further for easy analysis.
 def analyze_packets(connection_dict):
-    get_num_connections(connection_dict)
-    get_num_complete_connections(connection_dict)
+    total_cons = get_num_connections(connection_dict)
+    get_all_connections(connection_dict)
+    complete_cons = get_num_complete_connections(connection_dict)
+    get_num_reset_connections(connection_dict)
+    get_num_unclosed_connections(connection_dict)
+    get_num_start_open_connections(connection_dict)
+    get_connection_durations(connection_dict)
+    get_num_packs(connection_dict)
+    get_win_size(connection_dict)
+
+def get_win_size(connection_dict):
+    win_list = []
+    for tuple in connection_dict:
+        if is_complete(connection_dict[tuple]):
+            for packet in connection_dict[tuple]:
+                win_list.append(packet['window_size'])
+    print(f"Minimum receive window size including both send/received: {min(win_list)}")
+    print(f"Mean receive window size including both send/received: {(sum(win_list))/len(win_list)}")
+    print(f"Maximum receive window size including both send/received: {max(win_list)}\n")
+
+def get_num_packs(connection_dict):
+    packets_arr = []
+    for tuple in connection_dict.keys():
+        if is_complete(connection_dict[tuple]):
+            packets_arr.append(len(connection_dict[tuple]))
+    print(f"Minimum number of packets including both send/received: {min(packets_arr)}")
+    print(f"Mean number of packets including both send/received: {(sum(packets_arr))/len(packets_arr)}")
+    print(f"Maximum number of packets including both send/received: {max(packets_arr)}\n")
+    
+def get_connection_durations(connection_dict):
+    duration_list = []
+    starting = (connection_dict[next(iter(connection_dict))][0]['pkt_head']['ts_sec'],\
+               connection_dict[next(iter(connection_dict))][0]['pkt_head']['ts_usec'])
+    starting = starting[0] + starting[1]/1000000
+    for index, tuple in enumerate(connection_dict.keys()):
+
+        complete = is_complete(connection_dict[tuple])
+        if complete:
+            start_time, end_time = get_times(connection_dict[tuple], starting)
+            duration = end_time-start_time
+            duration_list.append(duration)
+    print("D) Complete TCP connections\n")
+    print(f"Minimum time duration: {min(duration_list)}")
+    print(f"Mean time duration: {(sum(duration_list))/(len(duration_list))}")
+    print(f"Maximum time duration: {max(duration_list)}\n")
+    return
+
+
+def get_num_unclosed_connections(connection_dict):
+    count = 0  
+
+    for conn_tuple, packets in connection_dict.items():
+        error = False 
+        start = False  
+
+        for packet in packets:
+            all_none = True
+            if packet['Flag_FIN'] == 1:
+                start = True  
+                error = False 
+
+            if start:  
+              
+
+                if len(packet['payload']) != 0: 
+                    error = True  
+
+        if error:  
+            count += 1
+
+    print(f"The number of TCP connections that were still open when the trace capture ended: {len(connection_dict) - count}")
+
+def get_num_start_open_connections(connection_dict):
+    total = 0
+    for tuple in connection_dict.keys():
+        if connection_dict[tuple][0]['Flag_SYN'] == 0:
+            total += 1
+    print(f"The number of TCP connections established before the capture started: {total}")
+    print("------------------------------------------------")
+
+def get_num_reset_connections(connection_dict):
+    total = 0
+    for tup in connection_dict.keys():
+        marked = False
+        for packet in connection_dict[tup]:
+            if (not marked) and (packet['Flag_RST'] == 1):
+                total += 1
+    print(f"The number of reset TCP connections: {total}")
+  
+
+def get_all_connections(connection_dict):
+    starting = (connection_dict[next(iter(connection_dict))][0]['pkt_head']['ts_sec'], connection_dict[next(iter(connection_dict))][0]['pkt_head']['ts_usec'])
+    starting = starting[0] + starting[1]/1000000
+
+    print("B) Connections' details:\n")
+
+    for index, tuple in enumerate(connection_dict.keys()):
+        #gets whether the tuple
+        complete = is_complete(connection_dict[tuple])
+        #gets a tuple representing the senders source and destination, IP and Port
+        source_tuple = get_sender(connection_dict, tuple)
+        print(f"Connection {index+1}:")
+        print(f"Source Adress: {byte_to_ip(source_tuple[2])}")
+        print(f"Destination Adress: {byte_to_ip(source_tuple[3])}")
+        print(f"Source Port: {source_tuple[0]}")
+        print(f"Destination Port: {source_tuple[1]}")
+        print(f"Status: {get_state(connection_dict[tuple])}")
+
+        if complete:
+            start_time, end_time = get_times(connection_dict[tuple], starting)
+            print(f"Start Time: {start_time}")
+            print(f"End Time: {end_time}")
+            print(f"Duration: {end_time - start_time}")
+            #gets the number of packets as a tuple from sender to receiver and receiver to sender
+            num_sender_pkt, num_receiver_pkt, send_data, rec_data = num_pckts(connection_dict[tuple], source_tuple)
+            total_num_pkt = len(connection_dict[tuple])
+            print(f"Number of packets sent from Source to Destination: {num_sender_pkt}")
+            print(f"Number of packets from Destination to Source: {num_receiver_pkt}")
+            print(f"Total number of packets: {total_num_pkt}")
+            print(f"Number of data bytes sent from Source to Destination: {len(send_data)}")
+            print(f"Number of data bytes sent from Destination to Source: {len(rec_data)}")
+            print(f"Total number of data bytes: {len(send_data) + len(rec_data)}")
+
+        print("++++++++++++++++++++++++++++++++++++++++++++++")
+
+    return
+
+def get_state(connection):
+    Syn = 0
+    Fin = 0
+    for pack in connection:
+        if pack['Flag_RST'] == 1:
+            return "R"
+        if pack['Flag_SYN'] == 1:
+            Syn+=1
+        if pack['Flag_FIN'] == 1:
+            Fin +=1
+    return f"S{Syn}F{Fin}"
+
+def get_times(connection, starting):
+    first_SYN = False
+    last_FIN = False
+    i = 0
+    while (not first_SYN):
+        if connection[i]['Flag_SYN']:
+            start = (connection[i]['pkt_head']['ts_sec'], connection[i]['pkt_head']['ts_usec'])
+            first_SYN = True
+        i += 1
+    j = len(connection) -1
+    while (not last_FIN):
+        if connection[j]['Flag_FIN']:
+            finish = (connection[j]['pkt_head']['ts_sec'], connection[j]['pkt_head']['ts_usec'])
+            last_FIN = True
+        j -= 1
+    return ((start[0]+start[1]/1000000) - starting, (finish[0]+finish[1]/1000000) - starting)
+
+
+def byte_to_ip(ip):
+    strin = ''
+    for index, byte in enumerate(ip):
+        strin += str(byte) + '.'
+    return strin[:-1]
+
+def num_pckts(connection, source_tuple):
+    src = 0
+    rec = 0
+    src_data = b''
+    rec_data = b''
+    for packet in connection:
+        if packet['tuple'] == source_tuple:
+            src += 1
+            src_data += packet['payload']
+        else:
+            rec += 1
+            rec_data += packet['payload']
+    return (src, rec, src_data, rec_data)
+
+def get_sender(connection_dict, tuple):
+    #format of sender: sender IP, sender Port, Receiver IP, Receiver Port
+    return (connection_dict[tuple][0]['src_port'],connection_dict[tuple][0]['dest_port'],\
+         connection_dict[tuple][0]['src_IP'], connection_dict[tuple][0]['dest_IP'])
+
+
+def is_complete(connection):
+    syn = False
+    fin = False
+    for packet in connection:
+        if packet["Flag_SYN"] == 1:
+            syn = True
+        if packet['Flag_FIN'] == 1:
+            fin = True
+    return (syn and fin)
+
+def get_connection_details():
+    return
 
 def get_num_connections(connection_dict):
-    print(len(connection_dict))
+    print("A) Total number of connections:", len(connection_dict))
+    print("\n----------------------------------------")
+    return len(connection_dict)
 
 def get_num_complete_connections(connection_dict):
     syn_start = False
@@ -32,8 +227,10 @@ def get_num_complete_connections(connection_dict):
                 if packet['Flag_FIN'] == 1:
                     syn_start = False
                     count += 1
-    print(len(connection_dict))
-    print(count)
+    print("----------------------------------------")
+    print("C) General\n")
+    print(f"Number of complete TCP connections: {count}")
+    return count
 
 
 
@@ -74,13 +271,16 @@ def organize_LD(filename: str) -> List[Dict]:
             #format of TCP tuple: src_port, rec_port, Seq #, ACK #, data offset/flags,
             #flags, window size, checksum, urgent pointer
             payload = LD_packs[index]['data'][offset:]
+            
 
-
+        LD_packs[index]['window_size'] = protocol_header[6]
         LD_packs[index]['eth_head'] = eth_head
         LD_packs[index]['IPv_head'] = IPv_head
         LD_packs[index]['trans_head'] = protocol_header
         LD_packs[index]['payload'] = payload
+       
         del(LD_packs[index]['data'])
+        #print(len(LD_packs[index]['payload']))
 
 
     LD_packs = break_up_packs(LD_packs, protocol)
@@ -104,11 +304,42 @@ def organize_LD(filename: str) -> List[Dict]:
     #sorts the packets by relative time
     connection_dict = sort_by_time(connection_dict)
 
-    #NOTe TO SELF, IF WE DO NOT HAVE FIN IN CONNECTION THEN 
+
+    #this sorts the connections by the appearance of the first packet sent in each
+    connection_dict = sort_connections(connection_dict)
+    
     
     analyze_packets(connection_dict)
 
     return LD_packs
+
+def sort_connections(connection_dict):
+    new_dict = {}
+    i = 0
+    changed = True
+    while changed:
+        changed = False
+        min_tuple = get_min(connection_dict)
+        if min_tuple != None:
+            new_dict[min_tuple] = connection_dict[min_tuple]
+            del connection_dict[min_tuple]
+            changed = True
+    return new_dict
+
+
+def get_min(connection_dict):
+    min_tuple = None
+    for tuple in connection_dict.keys():
+        if min_tuple != None:
+            if connection_dict[tuple][0]['pkt_head']['ts_sec'] < connection_dict[min_tuple][0]['pkt_head']['ts_sec']\
+                or (connection_dict[tuple][0]['pkt_head']['ts_sec'] == connection_dict[min_tuple][0]['pkt_head']['ts_sec']\
+                and connection_dict[tuple][0]['pkt_head']['ts_usec'] < connection_dict[min_tuple][0]['pkt_head']['ts_usec']):
+                min_tuple = tuple
+        else:
+            min_tuple = tuple
+
+    return min_tuple
+
 
 
 
